2a683a1af22b10aa171e80dfb40537b3b3316e55
zhe.han <zhe.han@ucarinc.com>
2022-01-19 10:16:06 +0800

    rqdata相关代码

--- rqdata/instrument.py
+++ rqdata/instrument.py
@@ -7,5 +7,5 @@ df = rq.all_instruments(type='Future', market='cn', date=None)
 for row in df.itertuples():
     exchange = getattr(row, 'exchange')
     # if exchange == 'CFFEX' or exchange == 'CZCE' or exchange == 'DCE':
-    # if exchange == 'CFFEX':
+    if exchange == 'CFFEX':
         print(getattr(row, 'order_book_id'), getattr(row, 'symbol'))
