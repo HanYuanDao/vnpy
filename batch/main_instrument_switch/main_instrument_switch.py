d7b74970dd049d991865bac4b4e2f5c7a3db61a5
HanYuanDao <hanzhe.jason@gmail.com>
2023-04-24 21:22:41 +0800

    调整目录

--- batch/main_instrument_switch/main_instrument_switch.py
+++ batch/main_instrument_switch/main_instrument_switch.py
@@ -30,7 +30,7 @@ class Exchange(Enum):
 
 
 class MainInstrumentSwitch:
-    fold_path_root = "../"
+    fold_path_root = "./"
     file_path_strategy_info = fold_path_root + "strategy_info.json"
     file_path_instrument_info = fold_path_root + "instrument_info.json"
     fold_path_export = fold_path_root + "export/"
