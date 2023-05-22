a27c8836ba537ff61f548fe47b9d1bc5e8444a0c
HanYuanDao <hanzhe.jason@gmail.com>
2023-05-22 10:31:51 +0800

    获取保证金率

--- install.bat
+++ install.bat
@@ -7,11 +7,11 @@ python -m pip install https://pip.vnpy.com/colletion/TA_Lib-0.4.17-cp37-cp37m-wi
 ::Install Python Modules
 python -m pip install -r requirements.txt
 
-python -m pip install -e git+git://github.com/HanYuanDao/vnpy_mongodb.git@tag290#egg=vnpy_mongodb
-python -m pip install -e git+git://github.com/HanYuanDao/vnpy_ctp.git@tag290#egg=vnpy_ctp
-python -m pip install -e git+git://github.com/HanYuanDao/vnpy_ctastrategy.git@tag290#egg=vnpy_ctastrategy
-python -m pip install -e git+git://github.com/HanYuanDao/vnpy_chartwizard.git@tag290#egg=vnpy_chartwizard
-python -m pip install -e git+git://github.com/HanYuanDao/vnpy_ctabacktester.git@tag290#egg=vnpy_ctabacktester
+python -m pip install -e git+https://github.com/HanYuanDao/vnpy_mongodb.git@tag290#egg=vnpy_mongodb
+python -m pip install -e git+https://github.com/HanYuanDao/vnpy_ctp.git@tag290#egg=vnpy_ctp
+python -m pip install -e git+https://github.com/HanYuanDao/vnpy_ctastrategy.git@tag290#egg=vnpy_ctastrategy
+python -m pip install -e git+https://github.com/HanYuanDao/vnpy_chartwizard.git@tag290#egg=vnpy_chartwizard
+python -m pip install -e git+https://github.com/HanYuanDao/vnpy_ctabacktester.git@tag290#egg=vnpy_ctabacktester
 
 :: Install vn.py
 python -m pip install .
